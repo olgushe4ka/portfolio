457e4e4820821d314cf2046dae7c8b5026d3e670
Olga <olgushe4ka@gmail.com>
2023-11-16 17:24:17 +0400

    new project added

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -546,4 +546,101 @@ export const portfolioInfo2 = [
       },
     ],
   },
+  {
+    id: 8,
+    title: "Дом 120м2",
+    mainFoto:
+      "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B0%D1%8F/2_Post.jpg",
+    otherFotos: [
+      {
+        roomName: "Гостиная",
+        photos: [
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B0%D1%8F/1_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B0%D1%8F/2_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B0%D1%8F/3_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B0%D1%8F/4_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B0%D1%8F/5_Post%20%281%29.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B0%D1%8F/8_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B0%D1%8F/6_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B0%D1%8F/7_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%BE%D1%81%D1%82%D0%B8%D0%BD%D0%B0%D1%8F/9_Post.jpg",
+        ],
+      },
+
+      {
+        roomName: "Кабинет",
+        photos: [
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82/1_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82/2_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82/3_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82/4_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82/12_Post.jpg",
+        ],
+      },
+      {
+        roomName: "Коридор",
+        photos: [
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%BE%D1%80%D0%B8%D0%B4%D0%BE%D1%80/1_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%BE%D1%80%D0%B8%D0%B4%D0%BE%D1%80/2_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%BE%D1%80%D0%B8%D0%B4%D0%BE%D1%80/3_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%BE%D1%80%D0%B8%D0%B4%D0%BE%D1%80/4_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%BE%D1%80%D0%B8%D0%B4%D0%BE%D1%80/8_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%BE%D1%80%D0%B8%D0%B4%D0%BE%D1%80/6_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%BE%D1%80%D0%B8%D0%B4%D0%BE%D1%80/7_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9A%D0%BE%D1%80%D0%B8%D0%B4%D0%BE%D1%80/9_Post.jpg",
+        ],
+      },
+      {
+        roomName: "Мастерская",
+        photos: [
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9C%D0%B0%D1%81%D1%82%D0%B5%D1%80%D1%81%D0%BA%D0%B0%D1%8F/1_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9C%D0%B0%D1%81%D1%82%D0%B5%D1%80%D1%81%D0%BA%D0%B0%D1%8F/4_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9C%D0%B0%D1%81%D1%82%D0%B5%D1%80%D1%81%D0%BA%D0%B0%D1%8F/3_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%9C%D0%B0%D1%81%D1%82%D0%B5%D1%80%D1%81%D0%BA%D0%B0%D1%8F/20000_Post.jpg",
+        ],
+      },
+      {
+        roomName: "Спальня",
+        photos: [
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%BF%D0%B0%D0%BB%D1%8C%D0%BD%D1%8F/1_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%BF%D0%B0%D0%BB%D1%8C%D0%BD%D1%8F/2_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%BF%D0%B0%D0%BB%D1%8C%D0%BD%D1%8F/3_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%BF%D0%B0%D0%BB%D1%8C%D0%BD%D1%8F/4_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%BF%D0%B0%D0%BB%D1%8C%D0%BD%D1%8F/50000_Post.jpg",
+        ],
+      },
+      {
+        roomName: "СУ",
+        photos: [
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%201%20%D1%8D%D1%82%D0%B0%D0%B6/1_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%201%20%D1%8D%D1%82%D0%B0%D0%B6/2_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%201%20%D1%8D%D1%82%D0%B0%D0%B6/3_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%201%20%D1%8D%D1%82%D0%B0%D0%B6/4_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%201%20%D1%8D%D1%82%D0%B0%D0%B6/5_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%201%20%D1%8D%D1%82%D0%B0%D0%B6/6_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%201%20%D1%8D%D1%82%D0%B0%D0%B6/7_Post.jpg",
+        ],
+      },
+      {
+        roomName: "СУ",
+        photos: [
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%202%20%D1%8D%D1%82%D0%B0%D0%B6/1_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%202%20%D1%8D%D1%82%D0%B0%D0%B6/2_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%202%20%D1%8D%D1%82%D0%B0%D0%B6/3_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%202%20%D1%8D%D1%82%D0%B0%D0%B6/4_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%202%20%D1%8D%D1%82%D0%B0%D0%B6/6_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%202%20%D1%8D%D1%82%D0%B0%D0%B6/7_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%A1%D0%A3%202%20%D1%8D%D1%82%D0%B0%D0%B6/8_Post.jpg",
+        ],
+      },
+      {
+        roomName: "Гардероб",
+        photos: [
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%B0%D1%80%D0%B4%D0%B5%D1%80%D0%BE%D0%B1/1_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%B0%D1%80%D0%B4%D0%B5%D1%80%D0%BE%D0%B1/3_Post.jpg",
+          "https://thumb.cloud.mail.ru/weblink/thumb/xw1/nKoY/moiZVdug3/%D0%A0%D0%B5%D0%BD%D0%B4%D0%B5%D1%80%D1%8B%20%D0%9A%D1%83%D1%8E%D0%BA%D0%B8/%D0%93%D0%B0%D1%80%D0%B4%D0%B5%D1%80%D0%BE%D0%B1/2_Post.jpg",
+        ],
+      },
+    ],
+  },
 ];
